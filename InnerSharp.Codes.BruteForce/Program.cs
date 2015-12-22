4594b62d512fe1ecfca2a85e9c8003c57e8bacb3
madhavan <madhavan.ekanathan@live.com.sg>
2015-12-22 14:00:01 +1300

    BruteForce

--- InnerSharp.Codes.BruteForce/Program.cs
+++ InnerSharp.Codes.BruteForce/Program.cs
@@ -10,6 +10,25 @@ namespace InnerSharp.Codes.BruteForce
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter line: ");
+            string str = Console.ReadLine();
+            Console.WriteLine("Enter Pattern to find in the above Line: ");
+            string pattern = Console.ReadLine();
+
+            BruteForce objBF = new BruteForce { Text = str.ToCharArray(), Pattern = pattern.ToCharArray() };
+
+            int indexOf = objBF.IndexOf(str.ToCharArray(), pattern.ToCharArray());
+
+            if(indexOf>-1)
+            {
+                Console.WriteLine(indexOf);
+            }
+            else
+            {
+                Console.WriteLine("Pattern Not Found");
+            }
+            Console.ReadLine();
         }
+        
     }
 }
